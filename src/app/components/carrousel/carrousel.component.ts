c33e61caca790aa1344ac63a08f7f2f9e7bcb3a3
Lukazukimo <lukazukimomr@gmail.com>
2021-04-08 04:25:00 -0300

    Fix Conflito IDs do HTML

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-carrousel',
   templateUrl: './carrousel.component.html',
   styleUrls: ['./carrousel.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class CarrouselComponent implements OnInit {
+  title = 'Wotaku';
+
   constructor() {}
 
   ngOnInit() {}
