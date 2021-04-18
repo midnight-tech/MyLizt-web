e0939b73865cd50746a6f0a5f78a85911f51b529
Lukazukimo <lukazukimomr@gmail.com>
2021-04-18 00:11:24 -0300

    Correcao de erro Motivo, mudanca na interface

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,33 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { search } from 'src/app/data/interfaces';
 import { HomeContextService } from 'src/app/services/home-context/home.service';
 
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
-  styleUrls: ['./catalogo.component.scss']
+  styleUrls: ['./catalogo.component.scss'],
 })
 export class CatalogoComponent implements OnInit {
-  type! : string
+  type!: search;
 
   constructor(
     public homeContext: HomeContextService,
-    public actRoute : ActivatedRoute,
+    public actRoute: ActivatedRoute
   ) {
     this.actRoute.params.subscribe((value) => {
       const { type } = value;
-      this.type = value.type.toUpperCase() as string
+      this.type = value.type.toUpperCase() as search;
       if (type == 'anime') {
-        homeContext.pageCatalogo(1,'ANIME',true)
+        homeContext.pageCatalogo(1, 'ANIME', true);
       } else if (type == 'serie') {
-        homeContext.pageCatalogo(1,'SERIE',true)
+        homeContext.pageCatalogo(1, 'SERIE', true);
       } else {
-        homeContext.pageCatalogo(1,'BOOK',true)
+        homeContext.pageCatalogo(1, 'BOOK', true);
       }
     });
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
