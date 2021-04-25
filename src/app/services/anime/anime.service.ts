f13cfca4b6d2a2df0a4f2935e43b57ffeeadd176
Adenilson Elias Da Silva <adenilson.elias2@gmail.com>
2021-04-25 16:53:21 -0300

    carroussel anime pegando os animes do dia

--- src/app/services/anime/anime.service.ts
+++ src/app/services/anime/anime.service.ts
@@ -4,78 +4,104 @@ import { AnimeCatalogo } from 'src/app/data/CatalogoAnime';
 import { CompleteAnime } from 'src/app/data/interfaces';
 import { environment } from 'src/environments/environment';
 
+const daysOfWeek = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+];
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimeService {
+  constructor() {}
 
-  constructor() { }
+  async getHomeCarroussel() : Promise<AnimeCatalogo[]> {
+    let day = daysOfWeek[new Date(Date.now()).getDay()];
 
-  async getHomeCarroussel() {
-    type request = {
-      results: AnimeCatalogo[]
-    }
-    let catalogo = await axios.get<request>("https://api.jikan.moe/v3/search/anime?q=&page=1&order_by=start_date&sort=desc&limit=5")
-    return catalogo.data.results.map((value) => new AnimeCatalogo(value, this))
+    let catalogo = await axios.get(`https://api.jikan.moe/v3/schedule/${day}`);
+
+    return catalogo.data[day].map((value : any) => {
+      value.airing = true;
+      value.start_date = value.airing_start;
+      return new AnimeCatalogo(value, this);
+    }).sort((a:any,b:any)=>{
+      if(a.score == b.score) return 0
+      if(a.score > b.score) return -1
+      return 1
+    }).slice(0,5);
   }
 
   async getCatalogo(page: number) {
     type request = {
-      results: AnimeCatalogo[]
-      last_page: number
-    }
-    let catalogo = await axios.get<request>(`https://api.jikan.moe/v3/search/anime?q=&page=${page}&order_by=start_date&sort=desc&limit=12`)
-    return { anime: catalogo.data.results.map((value) => new AnimeCatalogo(value, this)), lastPage: catalogo.data.last_page }
+      results: AnimeCatalogo[];
+      last_page: number;
+    };
+    let catalogo = await axios.get<request>(
+      `https://api.jikan.moe/v3/search/anime?q=&page=${page}&order_by=start_date&sort=desc&limit=12`
+    );
+    return {
+      anime: catalogo.data.results.map(
+        (value) => new AnimeCatalogo(value, this)
+      ),
+      lastPage: catalogo.data.last_page,
+    };
   }
 
   async partialSearch(query: string, isAll = false) {
-    const limit = isAll ? 9 : 3
+    const limit = isAll ? 9 : 3;
     type request = {
-      results: AnimeCatalogo[]
-    }
+      results: AnimeCatalogo[];
+    };
     try {
-      let result = await axios.get<request>(`https://api.jikan.moe/v3/search/anime?q=${query}&limit=${limit}`)
-      return result.data.results.map((value) => new AnimeCatalogo(value, this))
+      let result = await axios.get<request>(
+        `https://api.jikan.moe/v3/search/anime?q=${query}&limit=${limit}`
+      );
+      return result.data.results.map((value) => new AnimeCatalogo(value, this));
     } catch (e) {
-      console.error(e)
-      throw "Error"
+      console.error(e);
+      throw 'Error';
     }
   }
 
   async search(query: string, page: number = 1) {
     type request = {
-      results: AnimeCatalogo[],
-      last_page: number
-    }
-    const results = await axios.get<request>(`https://api.jikan.moe/v3/search/anime?q=${query}&limit=12&page=${page}`)
+      results: AnimeCatalogo[];
+      last_page: number;
+    };
+    const results = await axios.get<request>(
+      `https://api.jikan.moe/v3/search/anime?q=${query}&limit=12&page=${page}`
+    );
     return {
-      content: results.data.results.map((value) => new AnimeCatalogo(value, this)),
-      lastPage: results.data.last_page
-    }
+      content: results.data.results.map(
+        (value) => new AnimeCatalogo(value, this)
+      ),
+      lastPage: results.data.last_page,
+    };
   }
 
   async getAnimeComplete(id: number = 1, index?: number) {
     try {
       if (index) {
-        await this.wait(index * 800)
+        await this.wait(index * 800);
       }
-      const result = await axios.get<CompleteAnime>(`https://api.jikan.moe/v3/anime/${id}`)
-      return result.data
-    }
-    catch (e) {
+      const result = await axios.get<CompleteAnime>(
+        `https://api.jikan.moe/v3/anime/${id}`
+      );
+      return result.data;
+    } catch (e) {
       if (environment.production == false) {
-        console.error(e.message, e.code)
+        console.error(e.message, e.code);
       }
-      throw "Erro ao coletar as informaçoes deste anime"
+      throw 'Erro ao coletar as informaçoes deste anime';
     }
   }
 
   wait(ms: number) {
-    return new Promise(
-      (resolve, reject) => setTimeout(resolve, ms)
-    );
+    return new Promise((resolve, reject) => setTimeout(resolve, ms));
   }
-
-
 }
