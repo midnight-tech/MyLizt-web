3e3940ac186bc2aa43030b2173961c94c0cae726
Lukazukimo <lukazukimomr@gmail.com>
2021-05-04 02:13:39 -0300

    Media Query MenuLeft

--- src/app/components/menu-left/menu-left.component.ts
+++ src/app/components/menu-left/menu-left.component.ts
@@ -20,9 +20,9 @@ import { SerieService } from 'src/app/services/serie/serie.service';
 export class MenuLeftComponent implements OnInit {
   @Output() menuLeftEvent = new EventEmitter<boolean>();
 
-  anime: { anime: CompleteAnime; content: content; }[] = []
-  serie: { serie: CompleteSerie; content: content; }[] = []
-  book: { book: CompleteBook; content: content; }[] = []
+  anime: { anime: CompleteAnime; content: content }[] = [];
+  serie: { serie: CompleteSerie; content: content }[] = [];
+  book: { book: CompleteBook; content: content }[] = [];
 
   constructor(
     private listService: ListService,
@@ -40,29 +40,29 @@ export class MenuLeftComponent implements OnInit {
   getContents() {
     this.listService.getHomeContent().then(({ anime, book, serie }) => {
       let result = 0;
-      if(anime == undefined){
-        throw "content anime undefined"
+      if (anime == undefined) {
+        throw 'content anime undefined';
       }
       anime!.map(async (value, index) => {
         let animeMap = await this.animeService.getAnimeComplete(
           value.contentId as number,
           index
         );
-        this.anime.push({'anime' : animeMap , 'content':value});
+        this.anime.push({ anime: animeMap, content: value });
       });
 
       for (let i of book!!) {
         this.bookService
           .getBookComplete(i.contentId as string)
           .then((value) => {
-            this.book.push({'book' : value, 'content' : i});
+            this.book.push({ book: value, content: i });
           });
       }
       for (let i of serie!!) {
         this.serieService
           .getSerieComplete(i.contentId as number)
           .then((value) => {
-            this.serie.push({'serie' : value , 'content' : i});
+            this.serie.push({ serie: value, content: i });
           });
       }
     });
@@ -91,5 +91,9 @@ export class MenuLeftComponent implements OnInit {
       return;
     }
     this.router.navigate(['home', 'recommendations', type]);
+  }
+
+  closeMenu() {
+    this.menuLeftEvent.emit(false);
   }
 }
