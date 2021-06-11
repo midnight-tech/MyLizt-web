a76a3e9b357c0e40c3812d08918ff32dd94d2d8b
Adenilson Elias Da Silva <adenilson.elias2@gmail.com>
2021-06-11 01:38:03 -0300

    updates PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyLizt';
+
+  constructor(appUpdate: SwUpdate) {
+    if (!appUpdate.isEnabled) {
+      console.info("Not enabled update")
+      return;
+    }
+    appUpdate.available.subscribe((value) => {
+      appUpdate.activateUpdate().then(() => {
+        document.location.reload();
+      })
+    })
+  }
 }
