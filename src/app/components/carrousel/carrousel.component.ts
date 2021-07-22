40ab4895ec48b1a1500d3d2e542eee403c835fee
Lukazukimo <lukazukimomr@gmail.com>
2021-07-22 02:32:37 -0300

    Novo Carrousel

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+  AfterViewInit,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { CarrousselEntry } from 'src/app/data/interfaces';
 
@@ -8,20 +14,56 @@ import { CarrousselEntry } from 'src/app/data/interfaces';
   styleUrls: ['./carrousel.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class CarrouselComponent implements OnInit {
+export class CarrouselComponent implements OnInit, AfterViewInit {
   @Input() contents!: CarrousselEntry[];
 
   @Input() type!: string;
 
+  config = {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+  };
+
   constructor(private router: Router) {
-    console.log(window.innerWidth);
+    window.addEventListener('resize', () => {
+      this.mediaQueryCarrousel();
+    });
   }
 
+  ngAfterViewInit(): void {}
+
   ngOnInit() {
-    console.log('Eu sou diferente', window.innerWidth);
+    console.log(window.innerWidth);
+    setTimeout(() => {
+      this.mediaQueryCarrousel();
+    }, 0);
   }
 
   navigateToDetail(type: string, id: string) {
     this.router.navigate(['home', 'detail', type, id]);
+  }
+
+  mediaQueryCarrousel() {
+    if (window.innerWidth <= 400) {
+      this.config.slidesPerView = 1.25;
+      this.config.spaceBetween = 15;
+    } else if (window.innerWidth <= 600) {
+      this.config.slidesPerView = 1.5;
+      this.config.spaceBetween = 15;
+    } else if (window.innerWidth <= 800) {
+      this.config.slidesPerView = 2.25;
+      this.config.spaceBetween = 15;
+    } else if (window.innerWidth <= 1000) {
+      this.config.slidesPerView = 2.5;
+      this.config.spaceBetween = 30;
+    } else if (window.innerWidth <= 1200) {
+      this.config.slidesPerView = 3.25;
+      this.config.spaceBetween = 30;
+    } else {
+      this.config.slidesPerView = 4;
+      this.config.spaceBetween = 30;
+    }
   }
 }
