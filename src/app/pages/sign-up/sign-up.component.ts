a7bdda174787fa5aad54b8735e2e5059dfaf2556
Adenilson Elias Da Silva <adenilson.elias2@gmail.com>
2021-08-13 04:09:07 -0300

    popup dee erro na tela de signup

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { PopUpComponent } from 'src/app/components/pop-up/pop-up.component';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 const errorDefault = {
@@ -16,49 +17,32 @@ const errorDefault = {
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, AfterViewInit {
 
-  error
   username = new FormControl("")
   email = new FormControl("")
   password = new FormControl("")
   confirm_password = new FormControl("")
 
+  @ViewChild("popUpSignUp") popUpMenu?: PopUpComponent;
+
 
   constructor(private authService: AuthenticationService, private router: Router) {
-    this.error = errorDefault
-    this.username.valueChanges.subscribe((value) => {
-      this.resetError()
-    })
-    this.email.valueChanges.subscribe(() => {
-      this.resetError()
-    })
-    this.password.valueChanges.subscribe(() => {
-      this.resetError()
-    })
-    this.confirm_password.valueChanges.subscribe(() => {
-      this.resetError()
-    })
+  }
+
+  ngAfterViewInit(): void {
   }
 
   ngOnInit() {
   }
 
-  private resetError() {
-    this.error.username = ""
-    this.error.email = ""
-    this.error.password = ""
-    this.error.confirm_password = ""
-    this.error.other = ""
-  }
-
   register() {
     if (this.username.value.length >= 30) {
-      this.error.username = "Too many caracters"
+      this.popUpMenu?.showPopUp("Field Username has too many caracters")
       return
     }
     if (this.username.value.length == 0) {
-      this.error.username = "Field can not be empty"
+      this.popUpMenu?.showPopUp("Field Username can not be empty")
       return
     }
     if (this.password.value == this.confirm_password.value) {
@@ -67,15 +51,15 @@ export class SignUpComponent implements OnInit {
         switch (erro.code) {
           case 'auth/weak-password':
           case 'auth/email-already-in-use':
-            this.error.password = erro.message
+            this.popUpMenu?.showPopUp(erro.message)
             break
           case 'auth/invalid-email':
-            this.error.email = erro.message
+            this.popUpMenu?.showPopUp(erro.message)
             break
         }
       })
     } else {
-      this.error.confirm_password = "Password do not match"
+      this.popUpMenu?.showPopUp("Password do not match")
     }
   }
 
